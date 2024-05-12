7ee4fd5956c9c84fb3d941da18ebc4f63396dfba
Raju Mondal <mailmeraju92@gmail.com>
2024-05-12 16:12:32 +0000

    created the global state and required code settings for the theme works

--- multilevelCRM/client/src/App.js
+++ multilevelCRM/client/src/App.js
@@ -1,9 +1,16 @@
-
-
+ import {CaseBaseline, ThemeProvider} from "@mui/material"
+ import {createTheme} from "@mui/material/styles"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+ import {themeSettings} from "theme"
 function App() {
+  const mode = useSelector((state)=>state.global.mode)
+  const theme = useMemo(()=>createTheme(themeSettings(mode)),[mode])
   return (
     <div className="app">
-    
+      <ThemeProvider theme={theme}>
+         <CaseBaseline/>
+      </ThemeProvider>
     </div>
   );
 }
